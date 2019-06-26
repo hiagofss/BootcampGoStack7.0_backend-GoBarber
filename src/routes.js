9e3006aba59567b1a9a8ce5a58abb26391174dee
Hiago Filipe Santos de Souza <hiagofss98@gmail.com>
2019-06-25 22:11:26 -0300

    Criando loader de models

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  res.json({ message: 'Teste' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Hiago Souza',
+    email: 'hiagofss98@gmail.com',
+    password_hash: '123456789',
+  });
+  res.json(user);
 });
 
 export default routes;
