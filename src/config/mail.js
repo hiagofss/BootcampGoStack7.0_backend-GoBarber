e156d2af3a22ed6bb2eb4d00e5f0c262a843f35a
Hiago Souza <hiagofss98@gmail.com>
2019-06-30 20:47:46 -0300

    Configurando Nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '91b7059ea4581b',
+    pass: '10bfcea6033050',
+  },
+  default: {
+    from: 'Equipe Gobarber <noreply@gobarber.com>',
+  },
+};
