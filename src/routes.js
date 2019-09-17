3741aac5bea2bfcd492c7bc31d3bb484a1a5aeab
Hiago Souza <hiagofss98@gmail.com>
2019-09-16 21:10:54 -0300

    Add teste with Deploy

--- src/routes.js
+++ src/routes.js
@@ -16,6 +16,10 @@ import authMiddleware from './app/middlewares/auth';
 const routes = new Router();
 const upload = multer(multerConfig);
 
+routes.get('/', (req, res) => {
+  res.json({ ok: 'ok' });
+});
+
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
